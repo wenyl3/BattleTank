ebe3df844b539250395f1a09a9dad00e8847e003
wenyl3 <wenyl3@github.com>
2020-01-24 11:40:31 +0200

    Added basic prject files

---
+++ BattleTank/Intermediate/ProjectFiles/cmake-headers-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_HEADER_FILES 
+	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTank.h"
+	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTankGameModeBase.h"
+ )
+
---
+++ BattleTank/Intermediate/ProjectFiles/cmake-sources-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_SOURCE_FILES 
+	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTank.cpp"
+	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTankGameModeBase.cpp"
+ )
+
