1210761e27e988c44caef46ed0b89092e93c9220
wenyl3 <danielbabei.95@gmail.com>
2020-01-24 12:01:02 +0200

    Removed initial files

--- BattleTank/Intermediate/ProjectFiles/cmake-csharp-project.cmake
+++
@@ -1,4 +0,0 @@
-set(PROJECT_CSHARP_FILES 
-	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTank.Build.cs"
- )
-
--- BattleTank/Intermediate/ProjectFiles/cmake-sources-project.cmake
+++
@@ -1,5 +0,0 @@
-set(PROJECT_SOURCE_FILES 
-	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTank.cpp"
-	"/Files/danbabei/UnrealProjects/BattleTank/BattleTank/Source/BattleTank/BattleTankGameModeBase.cpp"
- )
-
