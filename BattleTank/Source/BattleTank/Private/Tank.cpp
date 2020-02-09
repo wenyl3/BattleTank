668ba8de21042cc9c9f05e9f2328b7872a296f75
wenyl3 <danielbabei.95@gmail.com>
2020-02-09 10:29:12 +0200

    Added TankAimingComponent

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -7,6 +7,8 @@ ATank::ATank()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -29,6 +31,5 @@ void ATank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 
 void ATank::AimAt(FVector hitLocation)
 {
-	auto tankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *tankName, *hitLocation.ToString());
+	TankAimingComponent->AimAt(hitLocation);
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -13,6 +14,9 @@ class BATTLETANK_API ATank : public APawn
 
 public:
 	void AimAt(FVector);
+
+protected:
+	UTankAimingComponent *TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
